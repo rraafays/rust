ac40f7fff433d875b3626747492e4b1680166982
raf <rraf@tuta.io>
2023-03-09 13:41:59 +0000

    chore: detecting build files

---
+++ the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-9d0c7ea159296a7c.d
@@ -0,0 +1,5 @@
+/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-9d0c7ea159296a7c.rmeta: src/main.rs
+
+/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-9d0c7ea159296a7c.d: src/main.rs
+
+src/main.rs:
