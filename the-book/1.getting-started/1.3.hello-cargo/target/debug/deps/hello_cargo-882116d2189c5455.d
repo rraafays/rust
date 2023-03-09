3a3f37a12cd7f1fea232411351e1e29caf2649f3
raf <rraf@tuta.io>
2023-03-09 13:43:34 +0000

    chore: ignored build files

--- the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-882116d2189c5455.d
+++
@@ -1,5 +0,0 @@
-/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-882116d2189c5455.rmeta: src/main.rs
-
-/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/debug/deps/hello_cargo-882116d2189c5455.d: src/main.rs
-
-src/main.rs:
