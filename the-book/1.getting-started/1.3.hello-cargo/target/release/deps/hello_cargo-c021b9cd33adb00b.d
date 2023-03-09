828b638a6b348d2f0d137ed94aa8fe57aa64a721
raf <rraf@tuta.io>
2023-03-09 14:03:38 +0000

    update: improved ignore

--- the-book/1.getting-started/1.3.hello-cargo/target/release/deps/hello_cargo-c021b9cd33adb00b.d
+++
@@ -1,5 +0,0 @@
-/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/release/deps/hello_cargo-c021b9cd33adb00b: src/main.rs
-
-/home/raf/Desarollo/rust/the-book/1.getting-started/1.3.hello-cargo/target/release/deps/hello_cargo-c021b9cd33adb00b.d: src/main.rs
-
-src/main.rs:
