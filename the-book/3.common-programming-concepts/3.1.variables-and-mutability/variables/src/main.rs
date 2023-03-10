b22479b632a5049f1e6785ffca9993a719bc2007
raf <rraf@tuta.io>
2023-03-10 13:19:58 +0000

    update: completed c3.2

--- the-book/3.common-programming-concepts/3.1.variables-and-mutability/variables/src/main.rs
+++ the-book/3.common-programming-concepts/3.1.variables-and-mutability/variables/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+const PI: f32 = 3.14; // constants can be declared in anyscope and can never change
+
+fn main() 
+{
+    let i = 10; // immutable variable i
+    println!("i is {i}");
+    // i = 11; // because i is immutable, subsequent assignments cause a panic
+    if i == 10 { println!("i is still {i}"); }
+    if i != 10 { println!("i is now {i}"); }
+
+    let mut x = 5; // mutable variable x
+    println!("x is {x}");
+    x = 6; // reassign x
+    println!("x is now {x}");
+
+    println!("pi to 2dp is {PI}");
+
+    let y = 5; // immutable variable y
+    let y = y + 1; // shadow y by redefining it with let
+    {
+        let y = y * 2; // shadowing only applies within the current scope
+        println!("the value of y in the innerscope is {y}"); // will give us 12
+    }
+    println!("the value of y is {y}"); // will give us 6
+    
+    let spaces = "    "; // immutable variable containing spaces
+    println!("look at those spaces {spaces}.");
+    let spaces = spaces.len();
+    println!("thats {spaces} whole spaces!");
+
+    // let mut spaces = "    "; // mutable variable containing spaces
+    // println!("look at those spaces {spaces}.");
+    // spaces = spaces.len(); // mutables cannot be reassigned to a datatype different from their initial datatype
+    // println!("thats {spaces} whole spaces!");
 }
