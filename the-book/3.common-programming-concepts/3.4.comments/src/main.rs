22ea2051114f8bcaae8218e9fad8f907a50d5bd1
raf <rraf@tuta.io>
2023-03-12 09:59:20 +0000

    update: learnt how to comment

---
+++ the-book/3.common-programming-concepts/3.4.comments/src/main.rs
@@ -0,0 +1,18 @@
+fn main() 
+{
+    // comment
+
+    /* comment */
+    
+    // comment
+    // on 
+    // multiple
+    // lines
+
+    /*
+     * comment
+     * on
+     * multiple
+     * lines
+     */
+}
