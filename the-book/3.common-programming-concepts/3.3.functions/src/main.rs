bc12ecb5abe41b31db7d6fa71a210e68b493e788
raf <rraf@tuta.io>
2023-03-12 09:57:48 +0000

    update: learnt about fuctions

--- the-book/3.common-programming-concepts/3.3.functions/src/main.rs
+++ the-book/3.common-programming-concepts/3.3.functions/src/main.rs
@@ -15,6 +15,24 @@ fn main()
 
     let height_cm: i32 = match height_cm.trim().parse() { Ok(number) => number, Err(_) => 0 };
     print_with_units(height_cm, "cm".to_string());
+
+    // statements are instructions that perform some action and do not return a value (void funtion)
+    // expressions evaluate to a resultant value (typedef function)
+
+    // let x = 7 is a statement not an expression
+    // let x = (let y = 7); panics because unlike other languages rust does not return anything
+    // for x to bind in this situation..
+    let x = { let y = 6; y + 1 }; // expressions within a body do not end in ;
+    println!("{x}"); 
+
+    let eight = eight();
+    println!("TEKKEN {eight}");
+    let jack_number = plus_plus(7);
+    println!("JACK-{jack_number}");
+    let mut future_number = 9;
+    increment(&mut future_number);
+    plus_plus(future_number);
+    println!("JACK-{future_number}");
 }
 
 fn test_even(x: i32)
@@ -24,7 +42,9 @@ fn test_even(x: i32)
     if x % 2 == 1 { println!("this number is odd") }
 }
 
-fn print_with_units(x: i32, unit: String)
-{
-    println!("{x}{unit}");
-}
+fn print_with_units(x: i32, unit: String) { println!("{x}{unit}"); }
+
+fn eight() -> i32 { 8 } // functions can return a value of a type if that type is declared with ->
+
+fn plus_plus(x: i32) -> i32 { x + 1 }
+fn increment(x: &mut i32) { *x += 1; }
