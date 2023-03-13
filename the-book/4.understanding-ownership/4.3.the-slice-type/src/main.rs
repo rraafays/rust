f7412ff3793fc6621f33df2bb4b953640e59579c
raf <rraf@tuta.io>
2023-03-13 21:07:00 +0000

    update: learning about slices, taking a break zZz

--- the-book/4.understanding-ownership/4.3.the-slice-type/src/main.rs
+++ the-book/4.understanding-ownership/4.3.the-slice-type/src/main.rs
@@ -1,17 +1,52 @@
 fn main() {
+  //                         0123456
+  //                               ￬
   let string = String::from("buenos noches");
-  let buenos = &string[0..6];
+  let buenos = &string[0..6]; // reference portions of a string
+  let noches = &string[7..13]; // .. refers to a range in this case 7 -> 13
+
+  let also_buenos = &string[..6]; // in rust we can also drop the 0 because .. assumes 0 by default
+  let also_noches = &string[7..]; // the opposite applies too! .. assumes the end
+  
+  let buenos_noches = &string[..]; // dropping both values references 0 -> end
+  let also_buenos_noches = &string; // this is equivalent to &string[..]
+
   println!("{buenos}");
+  println!("{noches}");
+
+  println!("{also_buenos}");
+  println!("{also_noches}");
+
+  println!("{buenos_noches}");
+  println!("{also_buenos_noches}");
+
+  let first_word = first_word(&string);
+  let second_word = second_word(&string);
+
+  println!("{first_word}");
+  println!("{second_word}");
 }
 
-fn first_word(string: &String) -> usize {
-  let bytes = string.as_bytes(); // convert string to byte array
+fn first_word(string: &String) -> &str {
+  let bytes = string.as_bytes();
 
-  for (i, &item) in bytes.iter().enumerate() { // iterate over the byte array
-    if item == b' ' { // b' ' is a byte literal that represents a space
-      return i; // return the index of the item if it is a space
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return &string[..i];
     }
   }
 
-  string.len() // return length
+  &string[..]
+}
+
+fn second_word(string: &String) -> &str {
+  let bytes = string.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return &string[i + 1..];
+    }
+  }
+
+  &string[..]
 }
